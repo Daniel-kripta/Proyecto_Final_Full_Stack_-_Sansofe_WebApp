cb18d04952eb55f9f4c9c7e59840d36d6022528c
Daniel-kripta <danielkriptaprofesional@gmail.com>
2026-06-11 21:05:52 +0000

    feat: Style Cards

--- apps/web/src/components/ArticuloCard/ArticuloCard.tsx
+++ apps/web/src/components/ArticuloCard/ArticuloCard.tsx
@@ -1,11 +1,12 @@
 import { Link } from 'react-router-dom'
+import styles from "./ArticuloCard.module.css"
 
 export function ArticuloCard({ headline, summary, publication, genre, id }: any) {
   return (
-    <div>
+    <div className={styles.cardPortada}>
       <Link to={`/articulo/${id}`}>
-        <p>{headline}</p>
-        {summary && <p>{summary}</p>}
+        <h3>{headline}</h3>
+        {summary && <><p>{summary}</p><span>Resumen generado por IA</span> <br/></> }
       </Link>
       <small>{publication}{genre ? ` · ${genre}` : ''}</small>
     </div>
